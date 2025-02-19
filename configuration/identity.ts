51a60090cc48a2444b88694cd56fb8ca4e05c6db
Guotai Tan <103235366+Fatreo@users.noreply.github.com>
2025-02-19 11:43:50 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Guotai`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `a MBA stduent in Kenan-Flagler Business School`;
 
-export const AI_NAME: string = `Price Deloitte Ernst Klynveld`;
+export const AI_NAME: string = `PDEK`;
 export const AI_TONE: string = `Precise,Serious`;
 export const AI_ROLE: string = `AI Assistant for Accounting Learning`;
