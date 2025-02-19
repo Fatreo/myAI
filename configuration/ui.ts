a266d0df80e360645b69afb83575404aa344efe8
Guotai Tan <103235366+Fatreo@users.noreply.github.com>
2025-02-19 11:41:57 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `Chat with PDEK`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const FOOTER_MESSAGE: string = `AI Chatbot for MBA742: Data Science and Artificial Intelligence in Business`;
+export const CLEAR_BUTTON_TEXT: string = `Clear`;
+export const PAGE_TITLE: string = `Guotai's AI Assistant`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, Guotai's AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
