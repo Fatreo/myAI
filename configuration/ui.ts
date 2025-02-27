3a5262f036745a574c715083abb8d092f78635f8
Guotai Tan <103235366+Fatreo@users.noreply.github.com>
2025-02-27 00:22:23 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Chat with Big D Guide`;
+export const CHAT_HEADER: string = `Big D Guide`;
 export const MESSAGE_PLACEHOLDER: string = `Howdy, y’all! Drop your message here and let’s giddy up!`;
 export const FOOTER_MESSAGE: string = `AI Chatbot for MBA742: Data Science and Artificial Intelligence in Business`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
