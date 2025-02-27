1fb5e77057bdb9d89ed60bc0a9182eac0a4e7781
Guotai Tan <103235366+Fatreo@users.noreply.github.com>
2025-02-27 00:29:31 -0500

    Update input.tsx

--- components/chat/input.tsx
+++ components/chat/input.tsx
@@ -52,7 +52,7 @@ export default function ChatInput({
                         className="border-none focus-visible:ring-0 focus-visible:ring-offset-0 bg-transparent"
                         onFocus={() => setIsFocused(true)}
                         onBlur={() => setIsFocused(false)}
-                        placeholder="Type your message here..."
+                        placeholder="Howdy, y’all! Drop your message here and let’s giddy up!"
                       />
                     </FormControl>
                   </FormItem>
